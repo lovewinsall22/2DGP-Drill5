71bc6d81e3d363dafb09e5a849880a62b6bf4318
lovewinsall22 <taehyun0222@naver.com>
2025-09-25 17:18:36 +0900

    드릴5 캐릭터 출력

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,14 +6,16 @@ ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
+frame = 0
 
-
-
+character_x, character_y = 400, 300
 
 while running:
     clear_canvas()
     ground.draw(800 // 2, 600 // 2, 800, 600)
+    character.clip_draw(frame * 100, 100, 100, 100, character_x, character_y)
     update_canvas()
+    frame = (frame + 1) % 8
     delay(0.05)
 
 
