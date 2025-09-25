3edd1a8fa1b9307efaaa4a4ed86ececfefe94998
lovewinsall22 <taehyun0222@naver.com>
2025-09-25 17:22:34 +0900

    드릴5 상하좌우 움직임 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -10,11 +10,35 @@ frame = 0
 
 character_x, character_y = 400, 300
 
+def handle_events():
+    global running
+    global character_x, character_y
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT: # button
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE: # esc
+                running = False
+            elif event.key == SDLK_RIGHT:
+                character_x += 10
+            elif event.key == SDLK_LEFT:
+                character_x -= 10
+            elif event.key == SDLK_UP:
+                character_y += 10
+            elif event.key == SDLK_DOWN:
+                character_y -= 10
+
+
+
+
 while running:
     clear_canvas()
     ground.draw(800 // 2, 600 // 2, 800, 600)
     character.clip_draw(frame * 100, 100, 100, 100, character_x, character_y)
     update_canvas()
+    handle_events()
     frame = (frame + 1) % 8
     delay(0.05)
 
