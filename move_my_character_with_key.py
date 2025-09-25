63c9ac88322d042080a942ddc0571029327f9024
lovewinsall22 <taehyun0222@naver.com>
2025-09-25 18:35:03 +0900

    드릴5 방향이동 개선구현 ( 연속 움직임 가능하게 함 )

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -9,34 +9,37 @@ running = True
 frame = 0
 
 character_x, character_y = 400, 300
-dir = 0
+dirX, dirY = 0, 0
+ifRight = 1
 
 def handle_events():
     global running
-    global character_x, character_y, dir
+    global character_x, character_y, dirX, dirY, ifRight
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT: # button
             running = False
         elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_ESCAPE: # esc
+            if event.key == SDLK_ESCAPE:  # esc
                 running = False
             elif event.key == SDLK_RIGHT:
-                character_x += 10
-                dir = 1
+                dirX += 1; ifRight = 1
             elif event.key == SDLK_LEFT:
-                character_x -= 10
-                dir = 0
+                dirX -= 1; ifRight = 0
             elif event.key == SDLK_UP:
-                character_y += 10
-                dir = 1
+                dirY += 1; ifRight = 1
             elif event.key == SDLK_DOWN:
-                character_y -= 10
-                dir = 0
+                dirY -= 1; ifRight = 0
         elif event.type == SDL_KEYUP:
-            if dir == 1: dir = 3
-            elif dir == 0: dir = 2
+            if event.key == SDLK_RIGHT:
+                dirX -= 1; ifRight = 3
+            elif event.key == SDLK_LEFT:
+                dirX += 1; ifRight = 2
+            elif event.key == SDLK_UP:
+                dirY -= 1; ifRight = 3
+            elif event.key == SDLK_DOWN:
+                dirY += 1; ifRight = 2
 
 
 
@@ -44,10 +47,12 @@ def handle_events():
 while running:
     clear_canvas()
     ground.draw(800 // 2, 600 // 2, 800, 600)
-    character.clip_draw(frame * 100, dir * 100 , 100, 100, character_x, character_y)
+    character.clip_draw(frame * 100, ifRight * 100 , 100, 100, character_x, character_y)
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
+    character_x += dirX * 5
+    character_y += dirY * 5
     delay(0.05)
 
 
