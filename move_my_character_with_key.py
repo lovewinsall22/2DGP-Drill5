03e4771c57f1b32728add94a6ba9adb9d1c515e4
lovewinsall22 <taehyun0222@naver.com>
2025-09-25 17:04:25 +0900

    드릴5 준비

---
+++ move_my_character_with_key.py
@@ -0,0 +1,16 @@
+from pico2d import *
+
+open_canvas()
+
+ground = load_image('ground.png')
+character = load_image('animation_sheet.png')
+
+running = True
+
+
+
+
+while running:
+    pass
+
+close_canvas()
