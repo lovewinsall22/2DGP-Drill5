3ec8af9203198de4ba3e0afc361452aceaa92ae5
lovewinsall22 <taehyun0222@naver.com>
2025-09-25 17:13:24 +0900

    드릴 5 배경 출력

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -2,7 +2,7 @@ from pico2d import *
 
 open_canvas()
 
-ground = load_image('ground.png')
+ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
@@ -11,6 +11,10 @@ running = True
 
 
 while running:
-    pass
+    clear_canvas()
+    ground.draw(800 // 2, 600 // 2, 800, 600)
+    update_canvas()
+    delay(0.05)
+
 
 close_canvas()
